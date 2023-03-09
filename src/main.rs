199f70855ec855fa9c79e04a0b45281e28062c12
Sami Korpela <skipadu@users.noreply.github.com>
2023-03-09 18:40:29 +0200

    Add player and horizontal movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,65 @@
 use bevy::prelude::*;
 
+const PLAYER_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
+const PLAYER_COLOR: Color = Color::rgb(0.1, 0.8, 0.3);
+const PLAYER_SPEED: f32 = 300.0;
+
+const TIME_STEP: f32 = 1.0 / 60.0;
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(player_movement)
+        .insert_resource(FixedTime::new_from_secs(TIME_STEP))
+        .run();
+}
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Collider;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Draw player, a green box
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: PLAYER_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: PLAYER_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Player,
+        Collider,
+    ));
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+    let mut player_transform = query.single_mut();
+    let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left) {
+        direction -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right) {
+        direction += 1.0;
+    }
+    let new_player_position = player_transform.translation.x + direction * PLAYER_SPEED * TIME_STEP;
+    player_transform.translation.x = new_player_position;
 }
