8a537699d256153a7d3b711b3f04af8217f9566d
Sami Korpela <skipadu@users.noreply.github.com>
2023-03-11 22:11:39 +0200

    Improve collision detection and separate obstacles and coins

--- src/main.rs
+++ src/main.rs
@@ -16,24 +16,11 @@ const COIN_COLOR: Color = Color::rgb(0.8, 0.7, 0.4);
 
 const TIME_STEP: f32 = 1.0 / 60.0;
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_event::<CollisionEvent>()
-        .add_systems(
-            (check_collision, player_movement.before(check_collision))
-                .in_schedule(CoreSchedule::FixedUpdate),
-        )
-        .insert_resource(FixedTime::new_from_secs(TIME_STEP))
-        .run();
-}
-
 #[derive(Component)]
 struct Player;
 
 #[derive(Component)]
-struct Collider;
+struct Collidable;
 
 #[derive(Component)]
 struct Obstacle;
@@ -41,8 +28,30 @@ struct Obstacle;
 #[derive(Component)]
 struct Coin;
 
-#[derive(Default)]
-struct CollisionEvent;
+enum CollisionEventType {
+    ObstacleCrash,
+    CoinCollect,
+}
+
+struct CollisionEvent(CollisionEventType);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_event::<CollisionEvent>()
+        .add_startup_system(setup)
+        .add_startup_system(setup_level)
+        .add_systems(
+            (
+                check_collision,
+                player_movement.before(check_collision),
+                play_collision_sound.after(check_collision),
+            )
+                .in_schedule(CoreSchedule::FixedUpdate),
+        )
+        .insert_resource(FixedTime::new_from_secs(TIME_STEP))
+        .run();
+}
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
@@ -62,78 +71,6 @@ fn setup(mut commands: Commands) {
             ..default()
         },
         Player,
-        // Collider,
-    ));
-    // Draw obstacle, a red box
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(50.0, OBSTACLE_SMALL_SIZE.y / 2.0, 0.0),
-                scale: OBSTACLE_SMALL_SIZE,
-                ..default()
-            },
-            sprite: Sprite {
-                color: OBSTACLE_COLOR,
-                ..default()
-            },
-            ..default()
-        },
-        Obstacle,
-        Collider,
-    ));
-
-    // "Coin" above obstacles to indicate when user has crossed
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(50.0, 20.0, 0.0),
-                scale: COIN_SIZE,
-                ..default()
-            },
-            sprite: Sprite {
-                color: COIN_COLOR,
-                ..default()
-            },
-            // visibility: Visibility::Hidden,
-            ..default()
-        },
-        Coin,
-        Collider,
-    ));
-
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(220.0, OBSTACLE_LARGE_SIZE.y / 2.0, 0.0),
-                scale: OBSTACLE_LARGE_SIZE,
-                ..default()
-            },
-            sprite: Sprite {
-                color: OBSTACLE_COLOR,
-                ..default()
-            },
-            ..default()
-        },
-        Obstacle,
-        Collider,
-    ));
-
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(220.0, 30.0, 0.0),
-                scale: COIN_SIZE,
-                ..default()
-            },
-            sprite: Sprite {
-                color: COIN_COLOR,
-                ..default()
-            },
-            // visibility: Visibility::Hidden,
-            ..default()
-        },
-        Coin,
-        Collider,
     ));
 }
 
@@ -155,39 +92,127 @@ fn player_movement(
 }
 
 fn check_collision(
-    mut commands: Commands,
     mut player_query: Query<(Entity, &Transform), With<Player>>,
-    collider_query: Query<(Entity, &Transform, Option<&Obstacle>), With<Collider>>,
-    coin_collider_query: Query<(Entity, &Transform, Option<&Coin>), With<Collider>>,
+    collider_query: Query<(Entity, &Transform, Option<&Obstacle>), With<Collidable>>,
+    coin_collider_query: Query<(Entity, &Transform, Option<&Coin>), With<Collidable>>,
     mut collision_events: EventWriter<CollisionEvent>,
 ) {
     let (_entity, player_transform) = player_query.single_mut();
     let player_size = player_transform.scale.truncate();
 
     // Check collision with player vs obstacles
-    for (_entity, transform, _maybe_obstacle) in &collider_query {
+    for (_entity, transform, maybe_obstacle) in &collider_query {
         let collision = collide(
             player_transform.translation,
             player_size,
             transform.translation,
             transform.scale.truncate(),
         );
-        if let Some(collision) = collision {
-            // TODO: for play sound system
-            // collision_events.send_default();
-            println!("COLLISION with Obstacle!");
+        if let Some(_c) = collision {
+            if maybe_obstacle.is_some() {
+                collision_events.send(CollisionEvent(CollisionEventType::ObstacleCrash));
+            }
         }
     }
     // Check collision with player vs coins
-    for (_entity, transform, _maybe_coin) in &coin_collider_query {
+    for (_entity, transform, maybe_coin) in &coin_collider_query {
         let collision = collide(
             player_transform.translation,
             player_size,
             transform.translation,
             transform.scale.truncate(),
         );
-        if let Some(collision) = collision {
-            println!("COLLISION with Coin!");
+        if let Some(_c) = collision {
+            if maybe_coin.is_some() {
+                collision_events.send(CollisionEvent(CollisionEventType::CoinCollect));
+            }
+        }
+    }
+}
+
+fn setup_level(mut commands: Commands) {
+    // Draw obstacle, a red box
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(50.0, OBSTACLE_SMALL_SIZE.y / 2.0, 0.0),
+                scale: OBSTACLE_SMALL_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: OBSTACLE_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Obstacle,
+        Collidable,
+    ));
+
+    // "Coin" above obstacles to indicate when user has crossed
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(50.0, 20.0, 0.0),
+                scale: COIN_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: COIN_COLOR,
+                ..default()
+            },
+            // visibility: Visibility::Hidden,
+            ..default()
+        },
+        Coin,
+        Collidable,
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(220.0, OBSTACLE_LARGE_SIZE.y / 2.0, 0.0),
+                scale: OBSTACLE_LARGE_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: OBSTACLE_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Obstacle,
+        Collidable,
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(220.0, 30.0, 0.0),
+                scale: COIN_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: COIN_COLOR,
+                ..default()
+            },
+            // visibility: Visibility::Hidden,
+            ..default()
+        },
+        Coin,
+        Collidable,
+    ));
+}
+
+fn play_collision_sound(mut collision_events: EventReader<CollisionEvent>) {
+    for event in collision_events.iter() {
+        match event {
+            CollisionEvent(CollisionEventType::ObstacleCrash) => {
+                println!("COLLISION with Obstacle!");
+            }
+            CollisionEvent(CollisionEventType::CoinCollect) => {
+                println!("COLLISION with Coin!");
+            }
         }
     }
 }
