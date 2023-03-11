2d2d26df6dbbe0501807188ec8c8f0c72c4cba3f
Sami Korpela <skipadu@users.noreply.github.com>
2023-03-11 11:19:49 +0200

    Add simple collision detection test, obstacles and coin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
 
 const PLAYER_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
 const PLAYER_COLOR: Color = Color::rgb(0.1, 0.8, 0.3);
-const PLAYER_SPEED: f32 = 300.0;
+const PLAYER_SPEED: f32 = 100.0;
+
+const OBSTACLE_COLOR: Color = Color::rgb(0.8, 0.2, 0.1);
+const OBSTACLE_SMALL_SIZE: Vec3 = Vec3::new(10.0, 10.0, 0.0);
+const OBSTACLE_LARGE_SIZE: Vec3 = Vec3::new(30.0, 20.0, 0.0);
+
+const COIN_SIZE: Vec3 = Vec3::new(5.0, 5.0, 0.0);
+const COIN_COLOR: Color = Color::rgb(0.8, 0.7, 0.4);
 
 const TIME_STEP: f32 = 1.0 / 60.0;
 
@@ -10,7 +20,11 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(player_movement)
+        .add_event::<CollisionEvent>()
+        .add_systems(
+            (check_collision, player_movement.before(check_collision))
+                .in_schedule(CoreSchedule::FixedUpdate),
+        )
         .insert_resource(FixedTime::new_from_secs(TIME_STEP))
         .run();
 }
@@ -21,18 +35,23 @@ struct Player;
 #[derive(Component)]
 struct Collider;
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+#[derive(Component)]
+struct Obstacle;
+
+#[derive(Component)]
+struct Coin;
+
+#[derive(Default)]
+struct CollisionEvent;
+
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     // Draw player, a green box
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
+                translation: Vec3::new(0.0, PLAYER_SIZE.y / 2.0, 0.0),
                 scale: PLAYER_SIZE,
                 ..default()
             },
@@ -43,6 +62,77 @@ fn setup(
             ..default()
         },
         Player,
+        // Collider,
+    ));
+    // Draw obstacle, a red box
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(50.0, OBSTACLE_SMALL_SIZE.y / 2.0, 0.0),
+                scale: OBSTACLE_SMALL_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: OBSTACLE_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Obstacle,
+        Collider,
+    ));
+
+    // "Coin" above obstacles to indicate when user has crossed
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(50.0, 20.0, 0.0),
+                scale: COIN_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: COIN_COLOR,
+                ..default()
+            },
+            // visibility: Visibility::Hidden,
+            ..default()
+        },
+        Coin,
+        Collider,
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(220.0, OBSTACLE_LARGE_SIZE.y / 2.0, 0.0),
+                scale: OBSTACLE_LARGE_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: OBSTACLE_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Obstacle,
+        Collider,
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(220.0, 30.0, 0.0),
+                scale: COIN_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: COIN_COLOR,
+                ..default()
+            },
+            // visibility: Visibility::Hidden,
+            ..default()
+        },
+        Coin,
         Collider,
     ));
 }
@@ -62,4 +152,42 @@ fn player_movement(
     }
     let new_player_position = player_transform.translation.x + direction * PLAYER_SPEED * TIME_STEP;
     player_transform.translation.x = new_player_position;
+}
+
+fn check_collision(
+    mut commands: Commands,
+    mut player_query: Query<(Entity, &Transform), With<Player>>,
+    collider_query: Query<(Entity, &Transform, Option<&Obstacle>), With<Collider>>,
+    coin_collider_query: Query<(Entity, &Transform, Option<&Coin>), With<Collider>>,
+    mut collision_events: EventWriter<CollisionEvent>,
+) {
+    let (_entity, player_transform) = player_query.single_mut();
+    let player_size = player_transform.scale.truncate();
+
+    // Check collision with player vs obstacles
+    for (_entity, transform, _maybe_obstacle) in &collider_query {
+        let collision = collide(
+            player_transform.translation,
+            player_size,
+            transform.translation,
+            transform.scale.truncate(),
+        );
+        if let Some(collision) = collision {
+            // TODO: for play sound system
+            // collision_events.send_default();
+            println!("COLLISION with Obstacle!");
+        }
+    }
+    // Check collision with player vs coins
+    for (_entity, transform, _maybe_coin) in &coin_collider_query {
+        let collision = collide(
+            player_transform.translation,
+            player_size,
+            transform.translation,
+            transform.scale.truncate(),
+        );
+        if let Some(collision) = collision {
+            println!("COLLISION with Coin!");
+        }
+    }
 }
